677bf6e5f8cb357c38de537868543011c4168a72
Christopher Mårtensson <cribalik@gmail.com>
2017-04-28 00:35:25 +0200

    Account for alignment in array

--- array/array.c
+++ array/array.c
@@ -1,32 +1,28 @@
-/* FIXME: alignment */
-
 #include <stdlib.h>
+#include <stddef.h>
 
 #define ARRAY_INITIAL_SIZE 4
 
-struct ArrayHeader {int length, capacity;};
+struct ArrayHeader {int length, capacity; union {size_t s; void* v; long l; long double d;} data;};
 
 static struct ArrayHeader* array_header(void* arr) {
-  return (struct ArrayHeader*) ((char*)arr - sizeof(struct ArrayHeader));
+  return (struct ArrayHeader*) ((char*)arr - offsetof(struct ArrayHeader, data));
 }
 
 void* array_internal_create(int item_size) {
   struct ArrayHeader* h;
-  void* result = (char*)malloc(ARRAY_INITIAL_SIZE * item_size + sizeof(struct ArrayHeader)) + sizeof(struct ArrayHeader);
-  h = array_header(result);
+  h = malloc(offsetof(struct ArrayHeader, data) +ARRAY_INITIAL_SIZE * item_size);
   h->length = 0;
   h->capacity = ARRAY_INITIAL_SIZE;
-  return result;
+  return &h->data;
 }
 
 void array_internal_push(void** arrp, int size) {
   struct ArrayHeader* h = array_header(*arrp);
-  if (h->length == h->capacity) {
-    array_header(*arrp)->capacity *= 2;
-    *arrp = ((char*) realloc(h, sizeof(struct ArrayHeader) + h->capacity*size) + sizeof(struct ArrayHeader));
-    h = array_header(*arrp);
+  if (h->length++ == h->capacity) {
+    h->capacity *= 2;
+    *arrp = &((struct ArrayHeader*)realloc(h, offsetof(struct ArrayHeader, data) + h->capacity*size))->data;
   }
-  ++h->length;
 }
 
 int* array_internal_len(void* a) {
