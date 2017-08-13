e2b82fc744e77a8a79e9d4bea43faf7e5f4c772c
Christopher Mårtensson <cribalik@gmail.com>
2017-08-13 14:28:36 +0200

    Some better naming

--- whisper/whisper.h
+++ whisper/whisper.h
@@ -4,17 +4,17 @@
 typedef int Whisper_TCPServer;
 typedef int Whisper_TCPConnection;
 
-int whisper_tcp_server_init(Whisper_TCPServer* r_out, short port);
-int whisper_tcp_server_init_ex(Whisper_TCPServer* r_out, short port, int non_blocking);
-int whisper_tcp_server_poll(Whisper_TCPServer r, Whisper_TCPConnection* c);
+int whisper_tcp_server_init(Whisper_TCPServer *server, short port);
+int whisper_tcp_server_init_ex(Whisper_TCPServer *server, short port, int non_blocking);
+int whisper_tcp_server_poll(Whisper_TCPServer server, Whisper_TCPConnection *connection);
 int whisper_tcp_server_close(Whisper_TCPServer r);
 
-int whisper_tcp_client(Whisper_TCPConnection* c_out, const char* hostname, short port);
+int whisper_tcp_client(Whisper_TCPConnection *c_out, const char *hostname, short port);
 
-int whisper_tcp_connection_write(Whisper_TCPConnection c, const char* data, int size);
-int whisper_tcp_connection_read(Whisper_TCPConnection c, char* out, int num_bytes);
-int whisper_tcp_connection_send(Whisper_TCPConnection c, const char* data, int num_bytes);
-int whisper_tcp_connection_receive(Whisper_TCPConnection c, char* out, int num_bytes);
+int whisper_tcp_connection_write(Whisper_TCPConnection c, const char *data, int size);
+int whisper_tcp_connection_read(Whisper_TCPConnection c, char *out, int num_bytes);
+int whisper_tcp_connection_send(Whisper_TCPConnection c, const char *data, int num_bytes);
+int whisper_tcp_connection_receive(Whisper_TCPConnection c, char *out, int num_bytes);
 int whisper_tcp_connection_flush(Whisper_TCPConnection c);
 int whisper_tcp_connection_close(Whisper_TCPConnection c);
 
